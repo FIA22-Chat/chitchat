b0b91ce6bd935478a2d7b577f6d6201b9fef3334
frkri <57220519+frkri@users.noreply.github.com>
2024-05-05 00:21:25 +0200

    Add Log4J logger (#15)
    * Add log4j dependencies
    
    * Add log4j config
    
    * Add log4j to server and client
    
    * Update run configs
    
    * Update client config

--- client/src/main/java/io/github/chitchat/client/HelloApplication.java
+++ client/src/main/java/io/github/chitchat/client/HelloApplication.java
@@ -5,19 +5,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HelloApplication extends Application {
+    private static final Logger log = LogManager.getLogger(HelloApplication.class);
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        log.info("Starting client...");
+
         FXMLLoader fxmlLoader =
                 new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.chitchat.client {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
+    requires org.apache.logging.log4j;
 
     opens io.github.chitchat.client to
             javafx.fxml;
