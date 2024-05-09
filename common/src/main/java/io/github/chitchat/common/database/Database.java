ff2beb8e8abea7a5ae165f9f1e46e2f1141ee810
frkri <57220519+frkri@users.noreply.github.com>
2024-05-09 16:16:44 +0200

    Improve database creation

--- common/src/main/java/io/github/chitchat/common/database/Database.java
+++ common/src/main/java/io/github/chitchat/common/database/Database.java
@@ -1,32 +1,41 @@
 package io.github.chitchat.common.database;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.nio.file.Path;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlite3.SQLitePlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class Database {
+    /** The name of the database file */
+    public static final String DB_NAME = "chitchat.db";
 
-    public static void createNewDatabase(String fileName) {
-        String url = "jdbc:sqlite:C:/sqlite/" + fileName;
+    private static final Logger log = LogManager.getLogger(Database.class);
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection(url);
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
+    /**
+     * Creates and configures a new SQLite database at the given path, the database file will be
+     * named as {@value DB_NAME}. Required migrations are automatically applied to the database. The
+     * caller is responsible for opening and closing the database connection.
+     *
+     * @param path the path to create the database at
+     * @return a new Jdbi instance
+     * @throws FlywayException if an error occurs while applying migrations
+     */
+    public static Jdbi create(@NotNull Path path) throws FlywayException {
+        String url = "jdbc:sqlite:" + path.resolve(DB_NAME).toAbsolutePath();
+        log.trace("Using database URL: {}", url);
 
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        createNewDatabase("test.db");
+        Flyway.configure()
+                .dataSource(url, null, null)
+                .locations("classpath:io/github/chitchat/common/migration")
+                .load()
+                .migrate();
+        return Jdbi.create(url)
+                .installPlugin(new SQLitePlugin())
+                .installPlugin(new SqlObjectPlugin());
     }
 }
