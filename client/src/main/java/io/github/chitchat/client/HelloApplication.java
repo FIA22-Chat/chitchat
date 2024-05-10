68551df2482646f33e6174ae5bcdb136115581f1
frkri <57220519+frkri@users.noreply.github.com>
2024-05-10 02:53:39 +0200

    Add db connection setup to client and server

--- client/src/main/java/io/github/chitchat/client/HelloApplication.java
+++ client/src/main/java/io/github/chitchat/client/HelloApplication.java
@@ -1,24 +1,45 @@
 package io.github.chitchat.client;
 
+import io.github.chitchat.common.Util;
+import io.github.chitchat.common.storage.database.Database;
 import java.io.IOException;
+import java.nio.file.Path;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jdbi.v3.sqlite3.SQLitePlugin;
+import org.sqlite.SQLiteDataSource;
 
 public class HelloApplication extends Application {
+    private static final String APP_NAME = "chitchat";
+    private static final String DB_NAME = APP_NAME + "_client.db";
     private static final Logger log = LogManager.getLogger(HelloApplication.class);
 
     public static void main(String[] args) {
+        log.info("Starting client...");
+        var db = Database.create(createDataSource(), new SQLitePlugin());
+
         launch();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static DataSource createDataSource() {
+        var dbPathStr = Util.getEnvOrDefault("DB_PATH", Util.getUserAppDir(APP_NAME).toString());
+        var dbPath = Path.of(dbPathStr, DB_NAME);
+        dbPath.toFile().getParentFile().mkdirs();
+
+        var datasource = new SQLiteDataSource();
+        datasource.setUrl("jdbc:sqlite:" + dbPath.toAbsolutePath());
+
+        return datasource;
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-        log.info("Starting client...");
-
         FXMLLoader fxmlLoader =
                 new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
--- server/src/main/java/io/github/chitchat/server/Main.java
+++ server/src/main/java/io/github/chitchat/server/Main.java
@@ -1,13 +1,35 @@
 package io.github.chitchat.server;
 
+import io.github.chitchat.common.Util;
+import io.github.chitchat.common.storage.database.Database;
+import java.nio.file.Path;
+import javax.sql.DataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jdbi.v3.sqlite3.SQLitePlugin;
+import org.sqlite.SQLiteDataSource;
 
 public class Main {
+    private static final String APP_NAME = "chitchat";
+    private static final String DB_NAME = APP_NAME + "_server.db";
     private static final Logger log = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Starting server...");
+        var db = Database.create(createDataSource(), new SQLitePlugin());
+
         new ServerC();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static DataSource createDataSource() {
+        var dbPathStr = Util.getEnvOrDefault("DB_PATH", Util.getUserAppDir(APP_NAME).toString());
+        var dbPath = Path.of(dbPathStr, DB_NAME);
+        dbPath.toFile().getParentFile().mkdirs();
+
+        var datasource = new SQLiteDataSource();
+        datasource.setUrl("jdbc:sqlite:" + dbPath.toAbsolutePath());
+
+        return datasource;
     }
 }
