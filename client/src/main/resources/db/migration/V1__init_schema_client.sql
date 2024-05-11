4f3e21b10c50e7753cb00aa03602500caf87c089
frkri <57220519+frkri@users.noreply.github.com>
2024-05-11 02:07:40 +0200

    Update schema to use blob for IDs

--- client/src/main/resources/db/migration/V1__init_schema_client.sql
+++ client/src/main/resources/db/migration/V1__init_schema_client.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS user
 (
     -- Id is expected to be unique and chronologically sortable (e.g. UUID v7)
     -- No need for a index, SQLite automatically creates a index for the primary key
-    id         integer NOT NULL UNIQUE PRIMARY KEY,
+    id         blob NOT NULL UNIQUE PRIMARY KEY,
     type       integer NOT NULL,
     -- Permission contains bitflags where each bit represents a permission scope
     -- Refer to the Java implementation for more details
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS user
 -- A group is a collection of users that can send messages to each other
 CREATE TABLE IF NOT EXISTS "group"
 (
-    id          integer NOT NULL UNIQUE PRIMARY KEY,
+    id          blob NOT NULL UNIQUE PRIMARY KEY,
     name        text    NOT NULL,
     description text    NOT NULL,
     modified_at integer NOT NULL
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS "group"
 -- Each message can be sent by a user to a specific group
 CREATE TABLE IF NOT EXISTS message
 (
-    id          integer NOT NULL UNIQUE PRIMARY KEY,
-    user_id     integer NOT NULL REFERENCES user (id),
-    group_id    text    NOT NULL REFERENCES "group" (id),
+    id          blob NOT NULL UNIQUE PRIMARY KEY,
+    user_id     blob NOT NULL REFERENCES user (id),
+    group_id    blob    NOT NULL REFERENCES "group" (id),
     type        integer NOT NULL,
     content     blob    NOT NULL,
     modified_at integer NOT NULL
@@ -39,8 +39,8 @@ CREATE INDEX IF NOT EXISTS message_group_id_index ON message (group_id);
 -- A user can be in multiple groups
 CREATE TABLE IF NOT EXISTS user_group
 (
-    user_id     integer NOT NULL REFERENCES user (id),
-    group_id    integer NOT NULL REFERENCES "group" (id),
+    user_id     blob NOT NULL REFERENCES user (id),
+    group_id    blob NOT NULL REFERENCES "group" (id),
     modified_at integer NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_group_user_id_index ON user_group (user_id);
@@ -49,8 +49,8 @@ CREATE INDEX IF NOT EXISTS user_group_group_id_index ON user_group (group_id);
 -- Each group can have multiple roles that are assigned to users
 CREATE TABLE IF NOT EXISTS role
 (
-    id          integer NOT NULL PRIMARY KEY,
-    group_id    integer NOT NULL REFERENCES "group" (id),
+    id          blob NOT NULL PRIMARY KEY,
+    group_id    blob NOT NULL REFERENCES "group" (id),
     name        text    NOT NULL,
     permission  integer NOT NULL,
     modified_at integer NOT NULL
@@ -60,8 +60,8 @@ CREATE INDEX IF NOT EXISTS role_group_id_index ON role (group_id);
 -- A user can have multiple roles in a specific group
 CREATE TABLE IF NOT EXISTS user_role
 (
-    user_id     integer NOT NULL REFERENCES user (id),
-    role_id     integer NOT NULL REFERENCES role (id),
+    user_id     blob NOT NULL REFERENCES user (id),
+    role_id     blob NOT NULL REFERENCES role (id),
     modified_at integer NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_role_user_id_index ON user_role (user_id);
--- server/src/main/resources/db/migration/V1__init_schema_server.sql
+++ server/src/main/resources/db/migration/V1__init_schema_server.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS user
 (
     -- Id is expected to be unique and chronologically sortable (e.g. UUID v7)
     -- No need for a index, SQLite automatically creates a index for the primary key
-    id          integer NOT NULL UNIQUE PRIMARY KEY,
+    id          blob NOT NULL UNIQUE PRIMARY KEY,
     type        integer NOT NULL,
     -- Permission contains bitflags where each bit represents a permission scope
     -- Refer to the Java implementation for more details
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS user
 -- Multiple sessions can be created for a user (e.g multiple devices)
 CREATE TABLE IF NOT EXISTS user_session
 (
-    user_id    integer NOT NULL REFERENCES user (id),
+    user_id    blob NOT NULL REFERENCES user (id),
     -- A temporary token that is used to authenticate the user
     token      text    NOT NULL,
     expires_at integer NOT NULL
@@ -33,7 +33,7 @@ CREATE INDEX IF NOT EXISTS user_session_user_id_index ON user_session (user_id);
 -- A group is a collection of users that can send messages to each other
 CREATE TABLE IF NOT EXISTS "group"
 (
-    id          integer NOT NULL UNIQUE PRIMARY KEY,
+    id          blob NOT NULL UNIQUE PRIMARY KEY,
     name        text    NOT NULL,
     description text    NOT NULL,
     modified_at integer NOT NULL
@@ -42,9 +42,9 @@ CREATE TABLE IF NOT EXISTS "group"
 -- Each message can be sent by a user to a specific group
 CREATE TABLE IF NOT EXISTS message
 (
-    id          integer NOT NULL UNIQUE PRIMARY KEY,
-    user_id     integer NOT NULL REFERENCES user (id),
-    group_id    text    NOT NULL REFERENCES "group" (id),
+    id          blob NOT NULL UNIQUE PRIMARY KEY,
+    user_id     blob NOT NULL REFERENCES user (id),
+    group_id    blob    NOT NULL REFERENCES "group" (id),
     type        integer NOT NULL,
     content     blob    NOT NULL,
     modified_at integer NOT NULL
@@ -55,8 +55,8 @@ CREATE INDEX IF NOT EXISTS message_group_id_index ON message (group_id);
 -- A user can be in multiple groups
 CREATE TABLE IF NOT EXISTS user_group
 (
-    user_id     integer NOT NULL REFERENCES user (id),
-    group_id    integer NOT NULL REFERENCES "group" (id),
+    user_id     blob NOT NULL REFERENCES user (id),
+    group_id    blob NOT NULL REFERENCES "group" (id),
     modified_at integer NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_group_user_id_index ON user_group (user_id);
@@ -65,8 +65,8 @@ CREATE INDEX IF NOT EXISTS user_group_group_id_index ON user_group (group_id);
 -- Each group can have multiple roles that are assigned to users
 CREATE TABLE IF NOT EXISTS role
 (
-    id          integer NOT NULL PRIMARY KEY,
-    group_id    integer NOT NULL REFERENCES "group" (id),
+    id          blob NOT NULL PRIMARY KEY,
+    group_id    blob NOT NULL REFERENCES "group" (id),
     name        text    NOT NULL,
     permission  integer NOT NULL,
     modified_at integer NOT NULL
@@ -76,8 +76,8 @@ CREATE INDEX IF NOT EXISTS role_group_id_index ON role (group_id);
 -- A user can have multiple roles in a specific group
 CREATE TABLE IF NOT EXISTS user_role
 (
-    user_id     integer NOT NULL REFERENCES user (id),
-    role_id     integer NOT NULL REFERENCES role (id),
+    user_id     blob NOT NULL REFERENCES user (id),
+    role_id     blob NOT NULL REFERENCES role (id),
     modified_at integer NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_role_user_id_index ON user_role (user_id);
