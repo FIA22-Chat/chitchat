2801ff4bf41281d0ed89159d0c79d6e76936678a
frkri <57220519+frkri@users.noreply.github.com>
2024-05-19 03:53:40 +0200

    Add icon pack

--- client/src/main/java/io/github/chitchat/client/HelloApplication.java
+++ client/src/main/java/io/github/chitchat/client/HelloApplication.java
@@ -1,18 +1,15 @@
 package io.github.chitchat.client;
 
 import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 public class HelloApplication extends Application {
     private static final Logger log = LogManager.getLogger(HelloApplication.class);
@@ -21,17 +18,31 @@ public class HelloApplication extends Application {
         launch();
     }
 
-
-     @Override
+    @Override
     public void start(Stage primaryStage) throws IOException {
-         log.info("Starting client...");
+        log.info("Starting client...");
 
-         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("pages/main/main.fxml"));
-         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
-         primaryStage.setTitle("Chat");
-         primaryStage.setScene(scene);
-         primaryStage.show();
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(HelloApplication.class.getResource("pages/main/main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        primaryStage.setTitle("Chat");
+        primaryStage.getIcons().addAll(getIcons());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    private Image @NotNull [] getIcons() {
+        var list = new Image[14];
+        var sizes = new int[] {16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 256};
 
+        log.trace("Loading {} icons", sizes.length);
+        for (int i = 0; i < sizes.length; i++)
+            list[i] =
+                    new Image(
+                            Objects.requireNonNull(
+                                    HelloApplication.class.getResourceAsStream(
+                                            "icons/logo/AppList.targetsize-" + sizes[i] + ".png")));
+
+        return list;
+    }
 }
