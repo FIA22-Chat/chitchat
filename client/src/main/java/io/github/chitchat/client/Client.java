537c8c66ae8b3989465acdfed7a997f5b907008e
frkri <57220519+frkri@users.noreply.github.com>
2024-05-20 21:03:37 +0200

    Remove dead code

--- client/src/main/java/io/github/chitchat/client/Client.java
+++
@@ -1,8 +0,0 @@
-package io.github.chitchat.client;
-
-public class Client {
-    public static void main(String[] args) {
-        System.out.println("io.github.chitchat.Client UP");
-        new ClientC();
-    }
-}
