cdf92c2e3c5789dd43fdb67d70e472c86207ef6a
H0kki <131407444+H0kki@users.noreply.github.com>
2024-05-22 19:35:31 +0200

    login gui + controller

--- client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
@@ -1,3 +1,20 @@
 package io.github.chitchat.client.view.pages.login;
 
-public class LoginController {}
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable
+{
+    @FXML
+    private TextField textFieldUser;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> textFieldUser.requestFocus());
+    }
+}
