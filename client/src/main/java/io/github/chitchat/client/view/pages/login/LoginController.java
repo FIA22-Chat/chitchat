b09d633e717ff85a2e2d40e122be1de6560f5f1a
frkri <57220519+frkri@users.noreply.github.com>
2024-05-25 04:55:23 +0200

    Apply formatting

--- client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
@@ -1,17 +1,14 @@
 package io.github.chitchat.client.view.pages.login;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class LoginController implements Initializable
-{
-    @FXML
-    private TextField textFieldUser;
+public class LoginController implements Initializable {
+    @FXML private TextField textFieldUser;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
