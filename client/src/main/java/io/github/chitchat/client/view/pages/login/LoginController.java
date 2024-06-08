ebb2b86d850c500f695504058d5906325cd9f6c5
frkri <57220519+frkri@users.noreply.github.com>
2024-06-08 20:47:33 +0200

    Rework show password button

--- client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/login/LoginController.java
@@ -6,38 +6,39 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.controlsfx.control.textfield.CustomPasswordField;
+import org.controlsfx.control.textfield.CustomTextField;
 
 @Log4j2
 @NoArgsConstructor
 public class LoginController implements Initializable {
-    @FXML private TextField textFieldUser;
-    @FXML private PasswordField textFieldPassword;
-    @FXML private Button loginButton;
+    @FXML private CustomTextField textFieldUser;
+    @FXML private CustomPasswordField textFieldPassword;
+    @FXML private Button buttonLogin;
 
-    private String password;
+    private String ephemeralPassword;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Platform.runLater(() -> textFieldUser.requestFocus());
 
-        loginButton.addEventFilter(
-                MouseEvent.MOUSE_PRESSED,
-                e -> {
-                    password = textFieldPassword.getText();
-                    textFieldPassword.clear();
-                    textFieldPassword.setPromptText(password);
-                });
-        loginButton.addEventFilter(
-                MouseEvent.MOUSE_RELEASED,
-                e -> {
-                    textFieldPassword.setText(password);
-                    textFieldPassword.setPromptText("Password");
-                });
+        textFieldPassword
+                .getRight()
+                .setOnMousePressed(
+                        _ -> {
+                            ephemeralPassword = textFieldPassword.getText();
+                            textFieldPassword.setPromptText(ephemeralPassword);
+                            textFieldPassword.clear();
+                        });
+        textFieldPassword
+                .getRight()
+                .setOnMouseReleased(
+                        _ -> {
+                            textFieldPassword.setText(ephemeralPassword);
+                            textFieldPassword.setPromptText("Password");
+                        });
     }
 
     public void login() {}
