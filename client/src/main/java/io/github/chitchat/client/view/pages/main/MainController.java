e86227fcfee272793b4a499da1c04ad1f91eb7d5
frkri <57220519+frkri@users.noreply.github.com>
2024-06-09 04:39:59 +0200

    Add ChitGroup to the main page ListView

--- client/src/main/java/io/github/chitchat/client/view/pages/main/MainController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/main/MainController.java
@@ -1,8 +1,11 @@
 package io.github.chitchat.client.view.pages.main;
 
+import io.github.chitchat.client.view.components.ChitGroup;
+import io.github.chitchat.client.view.components.ChitGroupCell;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,9 +15,9 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @NoArgsConstructor
-public class MainController {
+public class MainController implements Initializable {
     @FXML public Button callButton;
-    @FXML private ListView<String> chatList;
+    @FXML private ListView<ChitGroup> groupList;
 
     @FXML private TextArea chatinput;
 
@@ -25,8 +28,24 @@ public class MainController {
     @FXML private Label lastOnlineStatus;
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        groupList.setCellFactory(_ -> new ChitGroupCell());
+        groupList
+                .getItems()
+                .addAll(
+                        new ChitGroup(
+                                "aaaa",
+                                "aaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-256x.png"),
+                        new ChitGroup(
+                                "aaaa",
+                                "aaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-24x.png"),
+                        new ChitGroup(
+                                "aaaa",
+                                "aaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-60x.png"));
 
-        sendButton.setOnAction(event -> sendMessage());
+        sendButton.setOnAction(_ -> sendMessage());
     }
 
     @FXML
