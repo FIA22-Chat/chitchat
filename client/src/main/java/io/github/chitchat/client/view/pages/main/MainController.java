b3f35580a9665a64b42ce65cd30ecca5b3279689
LumixF <lukaslang2005@gmail.com>
2024-06-09 17:02:48 +0200

    Layout changed + implement MessageBox

--- client/src/main/java/io/github/chitchat/client/view/pages/main/MainController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/main/MainController.java
@@ -2,24 +2,32 @@ package io.github.chitchat.client.view.pages.main;
 
 import io.github.chitchat.client.view.components.ChitGroup;
 import io.github.chitchat.client.view.components.ChitGroupCell;
+import io.github.chitchat.client.view.components.ChitMessageBox;
+import io.github.chitchat.client.view.components.ChitMessageBoxCell;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @NoArgsConstructor
 public class MainController implements Initializable {
+    @FXML private ImageView selectedGroupImage;
     @FXML public Button callButton;
+    @FXML private Button callVideoButton;
+
     @FXML private ListView<ChitGroup> groupList;
 
-    @FXML private TextArea chatinput;
+    @FXML private ListView<ChitMessageBox> messageList;
 
     @FXML private TextArea inputArea;
 
@@ -29,6 +37,10 @@ public class MainController implements Initializable {
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         groupList.setCellFactory(_ -> new ChitGroupCell());
+        messageList.setCellFactory(_ -> new ChitMessageBoxCell());
+
+        selectedGroupImage.setImage(
+                new Image("/io/github/chitchat/client/assets/logo/logo-60x.png"));
         groupList
                 .getItems()
                 .addAll(
@@ -37,13 +49,32 @@ public class MainController implements Initializable {
                                 "aaaa",
                                 "/io/github/chitchat/client/assets/logo/logo-256x.png"),
                         new ChitGroup(
-                                "aaaa",
-                                "aaaa",
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                                "aaaaaaaaaaaa",
                                 "/io/github/chitchat/client/assets/logo/logo-24x.png"),
                         new ChitGroup(
                                 "aaaa",
-                                "aaaa",
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
                                 "/io/github/chitchat/client/assets/logo/logo-60x.png"));
+
+        messageList
+                .getItems()
+                .addAll(
+                        new ChitMessageBox(
+                                "aaaa",
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-256x.png",
+                                Pos.CENTER_LEFT),
+                        new ChitMessageBox(
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                                "aaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-256x.png",
+                                Pos.CENTER_RIGHT),
+                        new ChitMessageBox(
+                                "aaaa",
+                                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                                "/io/github/chitchat/client/assets/logo/logo-256x.png",
+                                Pos.CENTER_RIGHT));
 
         sendButton.setOnAction(_ -> sendMessage());
     }
