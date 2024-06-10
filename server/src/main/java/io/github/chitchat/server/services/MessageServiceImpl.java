0cc43b300ab69fb085b88588318e6dca17ad4743
frkri <57220519+frkri@users.noreply.github.com>
2024-06-11 00:15:13 +0200

    Add protobuf compiler dependency

---
+++ server/src/main/java/io/github/chitchat/server/services/MessageServiceImpl.java
@@ -0,0 +1,5 @@
+package io.github.chitchat.server.services;
+
+import io.github.chitchat.service.MessageServiceGrpc;
+
+public class MessageServiceImpl extends MessageServiceGrpc.MessageServiceImplBase {}
