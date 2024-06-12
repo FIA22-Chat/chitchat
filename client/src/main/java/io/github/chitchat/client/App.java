7e5c2da806ad79c85584d87329c3efb40391bc02
frkri <57220519+frkri@users.noreply.github.com>
2024-06-12 08:28:37 +0200

    Setup JavaFX (#22)
    Co-authored-by: rosenbergertobias <131407444+H0kki@users.noreply.github.com>
    Co-authored-by: LumixF <148453915+LumixF@users.noreply.github.com>
    Co-authored-by: LangLukas <LukasLang2005@gmail.com>

---
+++ client/src/main/java/io/github/chitchat/client/App.java
@@ -0,0 +1,74 @@
+package io.github.chitchat.client;
+
+import com.google.inject.Guice;
+import io.github.chitchat.client.config.Settings;
+import io.github.chitchat.client.modules.AppModule;
+import io.github.chitchat.client.modules.FrontendModule;
+import io.github.chitchat.client.modules.SettingsModule;
+import io.github.chitchat.client.view.routing.Page;
+import io.github.chitchat.client.view.routing.Router;
+import java.util.Objects;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
+
+@Log4j2
+public class App extends Application {
+    private static final String APP_NAME = "ChitChat";
+    private static final double STAGE_MIN_WIDTH = 300;
+    private static final double STAGE_MIN_HEIGHT = 300;
+
+    private Settings settings;
+    private Stage stage;
+
+    public static void main(String[] args) {
+        log.info("Starting client preloader...");
+
+        launch();
+    }
+
+    @Override
+    public void start(@NotNull Stage stage) throws InterruptedException {
+        log.info("Starting client GUI...");
+        var injector =
+                Guice.createInjector(
+                        new AppModule(APP_NAME), new FrontendModule(stage), new SettingsModule());
+        this.stage = stage;
+        this.settings = injector.getInstance(Settings.class);
+
+        var router = injector.getInstance(Router.class);
+        router.navigateTo(Page.LOGIN);
+
+        settings.applyStageSettings(stage);
+        stage.setMinWidth(STAGE_MIN_WIDTH);
+        stage.setMinHeight(STAGE_MIN_HEIGHT);
+        stage.getIcons().addAll(getIcons());
+        stage.setTitle(APP_NAME);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        log.info("Stopping client...");
+
+        settings.storeStageSettings(stage);
+        settings.save();
+    }
+
+    private Image @NotNull [] getIcons() {
+        var list = new Image[14];
+        var sizes = new int[] {16, 20, 24, 30, 32, 36, 40, 48, 60, 64, 72, 80, 96, 256};
+
+        log.trace("Loading {} icons", sizes.length);
+        for (var i = 0; i < sizes.length; i++)
+            list[i] =
+                    new Image(
+                            Objects.requireNonNull(
+                                    App.class.getResourceAsStream(
+                                            "assets/logo/logo-" + sizes[i] + "x.png")));
+
+        return list;
+    }
+}
---
+++ client/src/main/java/io/github/chitchat/client/config/SettingsData.java
@@ -0,0 +1,48 @@
+package io.github.chitchat.client.config;
+
+import java.io.Serial;
+import java.io.Serializable;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+@Data
+public class SettingsData implements Serializable {
+    @Serial private static final long serialVersionUID = -8954655507956526824L;
+    private static final Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+
+    private static final double DEFAULT_SCENE_WIDTH = 1400;
+    private static final double DEFAULT_SCENE_HEIGHT = 800;
+
+    private double sceneWidth = Math.min(screenBounds.getMaxX(), DEFAULT_SCENE_WIDTH);
+    private double sceneHeight = Math.min(screenBounds.getMaxY(), DEFAULT_SCENE_HEIGHT);
+    private double stageX = screenBounds.getMaxX() / 2 - sceneWidth / 2;
+    private double stageY = screenBounds.getMaxY() / 2 - sceneHeight / 2;
+    private boolean isMaximized;
+    private boolean isAlwaysOnTop;
+    private boolean isFullscreen;
+
+    private String username;
+
+    public void applyStageSettings(@NotNull Stage stage) {
+        stage.setX(this.getStageX());
+        stage.setY(this.getStageY());
+        stage.setWidth(this.getSceneWidth());
+        stage.setHeight(this.getSceneHeight());
+        stage.setMaximized(this.isMaximized());
+        stage.setAlwaysOnTop(this.isAlwaysOnTop());
+        stage.setFullScreen(this.isFullscreen());
+    }
+
+    public void storeStageSettings(@NotNull Stage stage) {
+        this.setStageX(stage.getX());
+        this.setStageY(stage.getY());
+        this.setSceneWidth(stage.getWidth());
+        this.setSceneHeight(stage.getHeight());
+        this.setMaximized(stage.isMaximized());
+        this.setAlwaysOnTop(stage.isAlwaysOnTop());
+        this.setFullscreen(stage.isFullScreen());
+    }
+}
