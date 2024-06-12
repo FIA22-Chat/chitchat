060b6ea7a08809d1fe36ee09bea52abf000d16f5
frkri <57220519+frkri@users.noreply.github.com>
2024-06-12 10:04:22 +0000

    Apply formatting

--- common/src/main/java/io/github/chitchat/common/filter/ProfanityFilter.java
+++ common/src/main/java/io/github/chitchat/common/filter/ProfanityFilter.java
@@ -3,38 +3,33 @@ package io.github.chitchat.common.filter;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
-
 public class ProfanityFilter {
 
     private final BloomFilter<CharSequence> bloomFilter;
 
-    public ProfanityFilter(int expectedInsertions, double falsePositiveRate)
-    {
+    public ProfanityFilter(int expectedInsertions, double falsePositiveRate) {
         bloomFilter = BloomFilter.create(Funnels.stringFunnel(null), expectedInsertions);
     }
-    public void addMsg(String message)
-    {
+
+    public void addMsg(String message) {
         bloomFilter.put(message);
     }
-    public boolean isSpam(String message)
-    {
+
+    public boolean isSpam(String message) {
         return bloomFilter.mightContain(message);
     }
 
-    public static String censor(String str)
-    {
-        if(str.length()<=2)
-        {
+    public static String censor(String str) {
+        if (str.length() <= 2) {
             return str;
         }
-        char firstLetter=str.charAt(0);
-        char lastLetter=str.charAt(str.length()-1);
+        char firstLetter = str.charAt(0);
+        char lastLetter = str.charAt(str.length() - 1);
         StringBuilder censor = new StringBuilder();
 
-        for (int i = 1;i<str.length()-1;i++)
-        {
+        for (int i = 1; i < str.length() - 1; i++) {
             censor.append('*');
         }
-        return firstLetter+censor.toString()+lastLetter;
+        return firstLetter + censor.toString() + lastLetter;
     }
 }
--- common/src/main/java/io/github/chitchat/common/filter/Spam.java
+++ common/src/main/java/io/github/chitchat/common/filter/Spam.java
@@ -1,4 +1,5 @@
 package io.github.chitchat.common.filter;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,36 +13,32 @@ public class Spam {
     private Map<String, Integer> msgCount;
     private Map<String, Long> lastMsgTimes;
 
-    public Spam()
-    {
+    public Spam() {
         msgCount = new HashMap<>();
         lastMsgTimes = new HashMap<>();
     }
 
-    public boolean isUserSpamming(String userid)
-    {
-        long currentTime = System.currentTimeMillis() /1000;
+    public boolean isUserSpamming(String userid) {
+        long currentTime = System.currentTimeMillis() / 1000;
 
         cleanOutdatedMessage(currentTime);
 
-        int currentCount = msgCount.getOrDefault(userid, 0) +1;
-        msgCount.put(userid,currentCount);
-        lastMsgTimes.put(userid,currentTime);
-
+        int currentCount = msgCount.getOrDefault(userid, 0) + 1;
+        msgCount.put(userid, currentCount);
+        lastMsgTimes.put(userid, currentTime);
 
         return currentTime > max_Msg_Normal;
     }
 
-    public boolean isAdminSpamming(String userid)
-    {
-        long currentTime = System.currentTimeMillis() /1000;
+    public boolean isAdminSpamming(String userid) {
+        long currentTime = System.currentTimeMillis() / 1000;
 
         cleanOutdatedMessage(currentTime);
 
-        int currentCount = msgCount.getOrDefault(userid, 0) +1;
-        msgCount.put(userid,currentCount);
-        lastMsgTimes.put(userid,currentTime);
-        //if user is spamming disable the textfield
+        int currentCount = msgCount.getOrDefault(userid, 0) + 1;
+        msgCount.put(userid, currentCount);
+        lastMsgTimes.put(userid, currentTime);
+        // if user is spamming disable the textfield
         return currentTime > max_Msg_Priv;
     }
 
@@ -54,35 +51,33 @@ public class Spam {
 
         int timeoutDuration = punishTime * offenseCount * 1000;
 
-//         SwingUtilities.invokeLater(() -> textField.setEnabled(false));
+        //         SwingUtilities.invokeLater(() -> textField.setEnabled(false));
 
-        new Thread(() -> {
-            try {
-                Thread.sleep(timeoutDuration);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }  {
-//                SwingUtilities.invokeLater(() -> textField.setEnabled(true));
-            }
-        }).start();
+        new Thread(
+                        () -> {
+                            try {
+                                Thread.sleep(timeoutDuration);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                            {
+                                //                SwingUtilities.invokeLater(() ->
+                                // textField.setEnabled(true));
+                            }
+                        })
+                .start();
 
         return true;
     }
 
-
-    public void cleanOutdatedMessage(long currentTime)
-    {
+    public void cleanOutdatedMessage(long currentTime) {
         long windowStart = currentTime - time;
-        for(String userid: lastMsgTimes.keySet())
-        {
+        for (String userid : lastMsgTimes.keySet()) {
             long lastMessageTime = lastMsgTimes.get(userid);
-            if(lastMessageTime<windowStart)
-            {
+            if (lastMessageTime < windowStart) {
                 msgCount.remove(userid);
                 lastMsgTimes.remove(userid);
             }
-
         }
-
     }
 }
