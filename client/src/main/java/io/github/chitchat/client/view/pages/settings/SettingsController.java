fdc027e0470a588a380fbc5b3cc257094bc8f13d
LangLukas <LukasLang2005@gmail.com>
2024-06-17 14:56:41 +0200

    settings changed

--- client/src/main/java/io/github/chitchat/client/view/pages/settings/SettingsController.java
+++ client/src/main/java/io/github/chitchat/client/view/pages/settings/SettingsController.java
@@ -1,13 +1,19 @@
 package io.github.chitchat.client.view.pages.settings;
 
 import io.github.chitchat.client.config.UserContext;
+import io.github.chitchat.client.view.routing.Page;
+import io.github.chitchat.client.view.routing.Router;
 import jakarta.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,12 +22,15 @@ import lombok.extern.log4j.Log4j2;
 @NoArgsConstructor
 public class SettingsController {
     @Inject private UserContext userContext;
-
+    @Inject private Router router;
     @FXML private Label headlineSettings;
     @FXML private TextField usernameField;
     @FXML private TextField statusField;
     @FXML private TextField imagePathField;
     @FXML private Button changeUsername;
+
+    @FXML private ImageView profileImageView;
+    @FXML private Circle profileCircle;
 
     @FXML private VBox profilePane;
     @FXML private VBox accountPane;
@@ -30,6 +39,10 @@ public class SettingsController {
 
     @FXML
     private void initialize() {
+        profileCircle = new Circle(50, 50, 50);
+        profileImageView.setClip(profileCircle);
+
+        profileImageView.setImage(new Image("io/github/chitchat/client/assets/logo/logo-256x.png"));
         usernameField.setText(userContext.getUsername());
     }
 
@@ -48,12 +61,22 @@ public class SettingsController {
         File selectedFile = fileChooser.showOpenDialog(null);
 
         if (selectedFile != null) {
-            imagePathField.setText(selectedFile.getAbsolutePath());
+            try {
+                Image image = new Image(selectedFile.toURI().toURL().toExternalForm());
+
+                profileImageView.setImage(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @FXML
     private void showProfile() {
         profilePane.setVisible(true);
+    }
+
+    public void backToMain() {
+        router.navigateTo(Page.MAIN);
     }
 }
