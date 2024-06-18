f8adce5d8c531c12d51e3f704ed0171b1e46c8ef
schnarrNoah <148444804+schnarrNoah@users.noreply.github.com>
2024-06-18 23:43:49 +0200

    Authentication (#34)
    Co-authored-by: SkyTrooper72 <148453272+SkyTrooper72@users.noreply.github.com>
    Co-authored-by: frkri <57220519+frkri@users.noreply.github.com>

---
+++ server/src/main/java/io/github/chitchat/server/auth/exceptions/AuthenticationFailedException.java
@@ -0,0 +1,6 @@
+package io.github.chitchat.server.auth.exceptions;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AuthenticationFailedException extends Exception {}
